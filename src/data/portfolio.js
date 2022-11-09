02c8106c25ca05416a6c40e28fb774286ea9fb3a
jcovington16 <covingtonjoshua7@gmail.com>
2022-11-08 22:56:36 -0700

    fix: changed image path.

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,13 +1,13 @@
 export default [
     {
         title: 'Marvel Meets',
-        imgUrl: 'src/assets/marvel-meets.png',
+        imgUrl: '../src/assets/marvel-meets.png',
         stack: ['BootStrap', 'Node', 'React', 'MongoDB', 'Twilio'],
         workUrl: 'marvel-meets-6ehlsutmy-jcovington16.vercel.app'
     },
     {
         title: 'Dad Jokes',
-        imgUrl: 'src/assets/Dadjokes.jpg',
+        imgUrl: '../src/assets/Dadjokes.jpg',
         stack: ['HTML', 'CSS', 'JavaScript', 'Basic API'],
         workUrl: 'https://50-projects50-days-4qx5tas3m-jcovington16.vercel.app'
     }
