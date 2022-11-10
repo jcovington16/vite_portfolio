ff708a520c9949e8c4962c46bd17375a70c07cd3
jcovington16 <covingtonjoshua7@gmail.com>
2022-11-10 10:32:22 -0700

    moving images around

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,13 +1,13 @@
 export default [
     {
         title: 'Marvel Meets',
-        imgUrl: 'public/marvel-meets.png',
+        imgUrl: '/marvel-meets.png',
         stack: ['BootStrap', 'Node', 'React', 'MongoDB', 'Twilio'],
         workUrl: 'https://marvel-meets-6ehlsutmy-jcovington16.vercel.app'
     },
     {
         title: 'Dad Jokes',
-        imgUrl: 'public/Dadjokes.jpg',
+        imgUrl: '/Dadjokes.jpg',
         stack: ['HTML', 'CSS', 'JavaScript', 'Basic API'],
         workUrl: 'https://50-projects50-days-4qx5tas3m-jcovington16.vercel.app'
     }
