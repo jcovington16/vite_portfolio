de812f4e47098842891b440e4803ebda210e821a
jcovington16 <covingtonjoshua7@gmail.com>
2022-11-20 20:56:05 -0700

    chore: added extra project.

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -10,5 +10,11 @@ export default [
         imgUrl: '/Dadjokes.jpg',
         stack: ['HTML', 'CSS', 'JavaScript', 'Basic API'],
         workUrl: 'https://50-projects50-days-4qx5tas3m-jcovington16.vercel.app'
+    },
+    {
+        title: 'NFT Card',
+        imgUrl: './nft.png',
+        stack: ['HTML', 'CSS'],
+        workUrl: 'https://nft-card-frontend-mentor-eta.vercel.app'
     }
 ];
